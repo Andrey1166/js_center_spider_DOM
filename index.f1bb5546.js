d79202218f758e31e3b6b358ef4a6705db8187b1
Andrey1166 <aannddrreeyy266@gmail.com>
2025-09-06 20:12:28 +0300

    make build

---
+++ index.f1bb5546.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider");e.addEventListener("load",function(){t&&e&&(e.clientWidth>0&&t.clientWidth>0||(t.style.position=t.style.position||"relative",e.style.position="absolute",e.style.left=(t.clientWidth-e.clientWidth)/2+"px",e.style.top=(t.clientHeight-e.clientHeight)/2+"px"))});
+//# sourceMappingURL=index.f1bb5546.js.map
